65a73af0466c95afc418e159769ba858b3d09109
Digital Minimal <jonathan.brunelle@digitalminimal.ca>
2021-09-30 08:24:51 -0400

    d3

---
+++ API_Challenge10/config.py
@@ -0,0 +1,15 @@
+twitter = {'API_Key':'BXxBA54AMvRGidxFCmRLwRRP2',
+            'API_Key_Secret':'EebZCnMGex9qoTzyyVm85Siwq52x0pLfWoqojF6CJi67uqAD37',
+            'Access_Token':'1437619313377218563-93Cq5SCjwGAA7e1gan4Al32RJ5JkNs',
+            'Access_Token_Secret':'WsyY9RdVPDz1mX1600rSIOFvuDiM6m899IPbRxzyaJlSr',
+'bearer':'AAAAAAAAAAAAAAAAAAAAAFjtTgEAAAAA49%2BPVBTD%2F5tzOouPjDXbngfpAJE%3DBlX8gcqNOgAwIEJHj33KXqytCsZGuOO7F9VavzwvinF04r78IS'
+
+
+
+
+          
+          
+          
+          }
+
+
